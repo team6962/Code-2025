03b85e51e88b54af0a824ddb9e3c905f00447efd
ThatCharlieK <86806912+ThatCharlieK@users.noreply.github.com>
2025-01-07 19:39:13 -0800

    Created basic Manipulator subsystem structure

--- src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
+++ src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
@@ -1,5 +1,63 @@
 package frc.robot.subsystems.manipulator;
 
-public class Manipulator {
+import com.revrobotics.spark.SparkMax;
+
+
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.Preferences;
+import frc.robot.Constants.Constants.ENABLED_SYSTEMS;
+
+// Manipulator is for the coral
+public class Manipulator extends SubsystemBase{
+    private SparkMax ManipulatorMotor;
+
+    public static enum State{
+        INTAKE,
+        OUTPUT,
+        OFF
+    }
+    public State state;
+
+    public Command setState(State state) {
+        return runEnd(
+            () -> this.state = state,
+            () -> this.state = State.OFF
+        );
+    }
+
+    @Override
+    public void periodic() {
+        if (!ENABLED_SYSTEMS.ENABLE_MANIPULATOR){ 
+            return;
+        }
+
+        if (RobotState.isDisabled()) {
+            state = State.OFF;
+        }
+
+        double motorPower = 0.0;
+
+        switch(state) {
+            case OFF:
+                motorPower = 0.0;
+                break;
+            case INTAKE:
+                motorPower = Preferences.MANIPULATOR.MANIPUALTOR_IN_SPEED;
+                break;
+            case OUTPUT:
+                motorPower = Preferences.MANIPULATOR.MANIPUALTOR_OUT_SPEED;
+                break;
+        }
+        
+        ManipulatorMotor.set(motorPower);
+
+        if (RobotContainer.getVoltage() < Preferences.VOLTAGE_LADDER.MANIPULATOR) {
+            ManipulatorMotor.stopMotor();
+        }
+    }
+
     
 }
