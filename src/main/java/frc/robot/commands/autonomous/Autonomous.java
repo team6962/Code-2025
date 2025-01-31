ba6055ffd2fdfc2e77e7711de626d13eb73a49b6
Brian <74162951+codeNinja-1@users.noreply.github.com>
2025-01-30 19:54:55 -0800

    Add algae pickup movement

--- src/main/java/frc/robot/commands/autonomous/Autonomous.java
+++ src/main/java/frc/robot/commands/autonomous/Autonomous.java
@@ -6,7 +6,9 @@ import com.team6962.lib.swerve.SwerveDrive;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.RobotStateController;
 
@@ -18,6 +20,12 @@ public class Autonomous extends SequentialCommandGroup {
         this.controller = controller;
         this.swerveDrive = swerveDrive;
 
+        addCommands(pathfindToProcessor());
+        addCommands(pickupPreplacedAlgae(0));
+        addCommands(pathfindToProcessor());
+        addCommands(pickupPreplacedAlgae(1));
+        addCommands(pathfindToProcessor());
+        addCommands(pickupPreplacedAlgae(2));
         addCommands(pathfindToProcessor());
     }
 
@@ -33,9 +41,23 @@ public class Autonomous extends SequentialCommandGroup {
         return null;
     }
 
+    private static Translation2d ALGAE_PICKUP_1 = new Translation2d(1.98, 2.18);
+    private static Translation2d ALGAE_PICKUP_2 = new Translation2d(1.21, 2.18);
 
-    public Command pickupAlgae() {
-        return null;
+    /**
+     * Pickup a pre-placed algae on the ground
+     * 
+     * @param algaePosition The position of the algae to pickup (0 is nearest
+     * the processor, and 2 is farthest)
+     * @return
+     */
+    public Command pickupPreplacedAlgae(int algaePosition) {
+        Translation2d offset = new Translation2d(0, 1.83 * algaePosition);
+
+        return Commands.sequence(
+            swerveDrive.pathfindTo(new Pose2d(ALGAE_PICKUP_1.plus(offset), Rotation2d.fromDegrees(-180))),
+            swerveDrive.pathfindTo(new Pose2d(ALGAE_PICKUP_2.plus(offset), Rotation2d.fromDegrees(-180)))
+        );
     }
     
     public boolean hasCoral() {
