d2bf4d1e991660a76fdb3d125294cb93dffb2d9b
Brian <74162951+codeNinja-1@users.noreply.github.com>
2025-02-21 13:26:41 -0800

    Add null check

--- src/main/java/frc/robot/commands/autonomous/AutoGeneration.java
+++ src/main/java/frc/robot/commands/autonomous/AutoGeneration.java
@@ -11,6 +11,7 @@ import com.team6962.lib.utils.MeasureMath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.autonomous.CoralSequences.CoralPosition;
 import frc.robot.util.software.Dashboard.AutonChooser;
 
@@ -206,6 +207,8 @@ public class AutoGeneration extends Thread {
             workRun = true;
 
             GeneratedAuto auto = autoGen.get();
+
+            if (auto == null) return Commands.none();
 
             CoralSequences.displaySequence("Autonomous Sequence", auto.getSequence(), auto.getParameters().startPose());
 
