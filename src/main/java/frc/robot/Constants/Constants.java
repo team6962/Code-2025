4cc4be92d301c5dddcc760a51389a57b5dc24c9c
Brian <74162951+codeNinja-1@users.noreply.github.com>
2025-02-28 11:25:33 -0800

    Tune elevator

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -279,7 +279,7 @@ public final class Constants {
       public static final Distance L2_HEIGHT = Inches.of(48.4);
       public static final Distance L3_HEIGHT = Inches.of(59.2);
       public static final Distance L4_HEIGHT = Inches.of(80.2);
-      public static final Distance INTAKE_HEIGHT = Inches.of(58.5);
+      public static final Distance INTAKE_HEIGHT = Inches.of(58.75);
     }
 
     public static final class ALGAE {
