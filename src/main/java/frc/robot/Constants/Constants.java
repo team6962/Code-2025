43834e420a13500d245b0a831dc9f8f16fe02cc8
NalluriRam <115056049+RamTheToad@users.noreply.github.com>
2025-03-05 14:18:18 -0800

    changed elevator gearing

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -242,8 +242,7 @@ public final class Constants {
 
   public static final class ELEVATOR {
     public static final double GEARING =
-              (3.0 / 1.0)
-            * (3.0 / 1.0)
+              (4.0 / 1.0)
             * (5.0 / 1.0)
             * (3.0 / 2.0)
             / 2.0; // CALCULATE LAST VALUE FOR STAGES IN THE ELEVATOR
