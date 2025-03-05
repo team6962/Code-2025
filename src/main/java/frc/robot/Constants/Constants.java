577050435b9121d364b1b666073e990e1cfb1f19
NalluriRam <115056049+RamTheToad@users.noreply.github.com>
2025-03-05 13:37:27 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants/Constants.java
+++ src/main/java/frc/robot/Constants/Constants.java
@@ -242,7 +242,7 @@ public final class Constants {
 
   public static final class ELEVATOR {
     public static final double GEARING =
-        (3.0 / 1.0)
+              (3.0 / 1.0)
             * (3.0 / 1.0)
             * (5.0 / 1.0)
             * (3.0 / 2.0)
