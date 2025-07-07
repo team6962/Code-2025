a38ebaa699f83017adebc5dd45d788eb9ee22546
Brian <74162951+codeNinja-1@users.noreply.github.com>
2025-07-07 14:34:12 -0700

    Limit switch and elevator progress

--- src/main/java/frc/robot/util/hardware/motion/BasedElevator.java
+++ src/main/java/frc/robot/util/hardware/motion/BasedElevator.java
@@ -1,24 +1,91 @@
 package frc.robot.util.hardware.motion;
 
-import com.badlogic.gdx.graphics.g3d.particles.ParticleSorter.Distance;
+import static edu.wpi.first.units.Units.Hertz;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
 
+import java.util.Set;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Frequency;
 import edu.wpi.first.units.measure.LinearAcceleration;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BasedElevator extends SubsystemBase {
+    /**
+     * Configuration for a BasedElevator.
+     */
     public static class Config {
+        /**
+         * The Proportional term of the PID controller.
+         */
         public double kP;
+
+        /**
+         * The Integral term of the PID controller.
+         */
         public double kI;
+
+        /**
+         * The Derivative term of the PID controller.
+         */
         public double kD;
+
+        /**
+         * The Static Feedforward term, which is the voltage required for the
+         * elevator to overcome static friction.
+         */
         public double kS;
-        public double kV;
-        public double kA;
+
+        /**
+         * The Gravity Feedforward term, which is the voltage required to
+         * overcome the force of gravity on the elevator.
+         */
         public double kG;
 
-        public LinearVelocity maxVelocity;
-        public LinearAcceleration maxAcceleration;
+        /**
+         * The Velocity Feedforward term, which is the coefficient multiplied
+         * by velocity to find the voltage required to maintain that velocity.
+         */
+        public double kV;
+
+        /**
+         * The Acceleration Feedforward term, which is the coefficient
+         * multiplied by acceleration to find the voltage required to reach
+         * that acceleration.
+         */
+        public double kA;
+
+        /**
+         * The maximum velocity that the elevator can travel updward.
+         */
+        public LinearVelocity maxUpwardVelocity;
+
+        /**
+         * The maximum upward acceleration that the elevator can achieve.
+         */
+        public LinearAcceleration maxUpwardAcceleration;
+
+        /**
+         * The maximum velocity that the elevator can travel downward.
+         */
+        public LinearVelocity maxDownwardVelocity;
+
+        /**
+         * The maximum downward acceleration that the elevator can achieve.
+         */
+        public LinearAcceleration maxDownwardAcceleration;
 
         public Distance minHeight;
         public Distance maxHeight;
@@ -32,6 +99,11 @@ public class BasedElevator extends SubsystemBase {
         public Current stallCurrentLimit;
 
         public Distance tolerance;
+
+        public LimitSwitchConfig floorLimitSwitch;
+        public LimitSwitchConfig ceilingLimitSwitch;
+
+        public Frequency updateFrequency = Hertz.of(50);
     }
 
     public static class MotorConfig {
@@ -43,10 +115,159 @@ public class BasedElevator extends SubsystemBase {
     public static class LimitSwitchConfig {
         public int dio;
         public LimitSwitchWiring wiring;
+        public boolean shouldSeedPosition;
     }
 
     public static enum LimitSwitchWiring {
         NormallyOpen,
         NormallyClosed
+    }
+
+    private Config config;
+
+    private BasedMotor[] motors;
+    private BasedLimitSwitch floor;
+    private BasedLimitSwitch ceiling;
+
+    private ElevatorFeedforward feedforward;
+    private TrapezoidProfile downProfile;
+    private TrapezoidProfile upProfile;
+
+    private boolean absolutePositionSeeded = false;
+
+    public BasedElevator(Config config) {
+        this.config = config;
+
+        motors = new BasedMotor[config.motors.length];
+        for (int i = 0; i < config.motors.length; i++) {
+            motors[i] = new BasedMotor(config, config.motors[i]);
+        }
+
+        floor = new BasedLimitSwitch(config.floorLimitSwitch);
+        ceiling = new BasedLimitSwitch(config.ceilingLimitSwitch);
+
+        feedforward = new ElevatorFeedforward(config.kS, config.kG, config.kV, config.kA);
+
+        upProfile = new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(
+                config.maxUpwardVelocity.in(MetersPerSecond),
+                config.maxUpwardAcceleration.in(MetersPerSecondPerSecond)
+            )
+        );
+
+        downProfile = new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(
+                config.maxDownwardVelocity.in(MetersPerSecond),
+                config.maxDownwardAcceleration.in(MetersPerSecondPerSecond)
+            )
+        );
+    }
+
+    public Distance getPosition() {
+        double positionSum = 0;
+
+        for (BasedMotor motor : motors) {
+            positionSum += motor.getPosition().in(Meters);
+        }
+
+        return Meters.of(positionSum / motors.length);
+    }
+
+    public LinearVelocity getVelocity() {
+        double velocitySum = 0;
+
+        for (BasedMotor motor : motors) {
+            velocitySum += motor.getVelocity().in(MetersPerSecond);
+        }
+
+        return MetersPerSecond.of(velocitySum / motors.length);
+    }
+
+    private Command moveTo(Distance targetPosition, LinearVelocity targetVelocity, boolean endWithinTolerance) {
+        return Commands.defer(() -> new Command() {
+            private TrapezoidProfile.State initialState;
+
+            private TrapezoidProfile.State goalState = new TrapezoidProfile.State(
+                targetPosition.in(Meters), targetVelocity.in(MetersPerSecond)
+            );
+
+            private double startTime;
+
+            @Override
+            public void initialize() {
+                initialState = new TrapezoidProfile.State(
+                    getPosition().in(Meters), getVelocity().in(MetersPerSecond)
+                );
+
+                startTime = Timer.getFPGATimestamp();
+            }
+
+            @Override
+            public void execute() {
+                double currentTime = Timer.getFPGATimestamp() - startTime;
+
+                TrapezoidProfile profile = goalState.position < initialState.position ? downProfile : upProfile;
+
+                TrapezoidProfile.State currentState = profile.calculate(
+                    currentTime,
+                    initialState,
+                    goalState
+                );
+
+                TrapezoidProfile.State nextState = profile.calculate(
+                    currentTime + config.updateFrequency.asPeriod().in(Seconds),
+                    initialState,
+                    goalState
+                );
+
+                Voltage feedforwardVoltage = Volts.of(feedforward.calculateWithVelocities(currentState.velocity, nextState.velocity));
+
+                throw new UnsupportedOperationException("Need to add motor control calls here");
+            }
+
+            @Override
+            public boolean isFinished() {
+                return endWithinTolerance && getPosition().minus(targetPosition).abs(Meters) < config.tolerance.in(Meters);
+            }
+        }, Set.of(this));
+    }
+
+    public Command moveTo(Distance targetPosition, LinearVelocity targetVelocity) {
+        return moveTo(targetPosition, targetVelocity, true);
+    }
+
+    public Command moveTo(Distance targetPosition) {
+        return moveTo(targetPosition, MetersPerSecond.of(0), true);
+    }
+
+    public Command holdAt(Distance targetPosition) {
+        return moveTo(targetPosition, MetersPerSecond.of(0), false);
+    }
+
+    public Command hold() {
+        return Commands.defer(() -> holdAt(getPosition()), Set.of(this));
+    }
+
+    private void setPosition(Distance position) {
+        for (BasedMotor motor : motors) {
+            motor.setPosition(position);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        Distance absolutePosition = null;
+
+        if (config.floorLimitSwitch.shouldSeedPosition && floor.isPressed()) {
+            absolutePosition = config.minHeight;
+        } else if (config.ceilingLimitSwitch.shouldSeedPosition && ceiling.isPressed()) {
+            absolutePosition = config.maxHeight;
+        }
+
+        if (absolutePosition != null) {
+            absolutePositionSeeded = true;
+
+            setPosition(absolutePosition);
+        }
     }
 }
