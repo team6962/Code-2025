e2628e8c66e6c39cd38f8d160b368180ecd6a834
RamTheToad <115056049+RamTheToad@users.noreply.github.com>
2025-07-07 14:36:21 -0700

    upgraded the base of BasedMotor

--- src/main/java/frc/robot/util/hardware/motion/BasedMotor.java
+++ src/main/java/frc/robot/util/hardware/motion/BasedMotor.java
@@ -1,5 +1,6 @@
 package frc.robot.util.hardware.motion;
 
+import static edu.wpi.first.units.Units.Meter;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
@@ -11,6 +12,10 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.util.hardware.SparkMaxUtil;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -25,8 +30,14 @@ public class BasedMotor extends SubsystemBase {
         motor = new SparkMax(motorConfig.canId, MotorType.kBrushless);
         encoder = motor.getEncoder();
         PID = motor.getClosedLoopController();
-        // configurePID
-        
+
+        SparkMaxConfig sparkConfig = new SparkMaxConfig();
+        sparkConfig.closedLoop.minOutput((-12. + elevatorConfig.kG) / 12.);
+        SparkMaxUtil.configure(sparkConfig, true, IdleMode.kBrake, 80, 60);
+        SparkMaxUtil.configureEncoder(sparkConfig, elevatorConfig.sprocketRadius.in(Meters) / elevatorConfig.gearReduction); // replace with conversion factor
+        SparkMaxUtil.configurePID(
+            sparkConfig, elevatorConfig.kP, elevatorConfig.kI, elevatorConfig.kD, elevatorConfig.kV, elevatorConfig.minHeight.in(Meters),elevatorConfig.maxHeight.in(Meters), false);
+        SparkMaxUtil.saveAndLog(this, motor, sparkConfig);
     }   
 
     public void setPosition(Distance Height) {
@@ -52,10 +63,9 @@ public class BasedMotor extends SubsystemBase {
     public double getDutyCycle(){
         return motor.getAppliedOutput();
     }
-    public void configurePID(){
-        // 
-    }
 
-    //getvelocity
+    public void run(double pidSetpoint, Voltage feedforwardVoltage) {
+        // PID.setReference(pidSetpoint, feedforwardVoltage);
+    }
     //run with a pid setpoint and a feedforward voltage
 }
