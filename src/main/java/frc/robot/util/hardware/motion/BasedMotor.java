aabfe2bb8fc02982b3d410fa88c2f87760fd30c2
RamTheToad <115056049+RamTheToad@users.noreply.github.com>
2025-07-07 14:21:43 -0700

    made base structure of basedmotor

--- src/main/java/frc/robot/util/hardware/motion/BasedMotor.java
+++ src/main/java/frc/robot/util/hardware/motion/BasedMotor.java
@@ -1,12 +1,59 @@
 package frc.robot.util.hardware.motion;
 
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Voltage;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BasedMotor extends SubsystemBase {
-    public BasedElevator.Config motorConfig;
-    public String name;
+    private BasedElevator.MotorConfig motorConfig;
+    private SparkMax motor;
+    private RelativeEncoder encoder;
+    private SparkClosedLoopController PID;
 
     public BasedMotor(BasedElevator.Config elevatorConfig, BasedElevator.MotorConfig motorConfig){
         this.motorConfig = motorConfig;
+        motor = new SparkMax(motorConfig.canId, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+        PID = motor.getClosedLoopController();
+        // configurePID
+        
     }   
+
+    public void setPosition(Distance Height) {
+        encoder.setPosition(Height.in(Meters));
+    }
+
+    public Distance getPosition(){
+        return Meters.of(encoder.getPosition());
+    }
+
+    public void setVelocity(double Velocity){
+        motor.set(Velocity);
+    }
+
+    public void stop() {
+        motor.stopMotor();
+    }
+
+    public void setVoltage(Voltage voltage) {
+        motor.setVoltage(voltage);
+    }
+
+    public double getDutyCycle(){
+        return motor.getAppliedOutput();
+    }
+    public void configurePID(){
+        // 
+    }
+
+    //getvelocity
+    //run with a pid setpoint and a feedforward voltage
 }
