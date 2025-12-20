d51a70341df22be0e9f51345c8306bb5836c1e50
Brian <74162951+codeNinja-1@users.noreply.github.com>
2025-12-20 11:57:32 -0800

    Remove revlib

--- src/main/java/com/team6962/lib/telemetry/StatusChecks.java
+++ src/main/java/com/team6962/lib/telemetry/StatusChecks.java
@@ -1,21 +1,21 @@
 package com.team6962.lib.telemetry;
 
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.spark.SparkMax;
-import com.team6962.lib.utils.CTREUtils;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.team6962.lib.utils.CTREUtils;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
  * {@code StatusChecks} is a utility class that provides a way to add boolean status checks to
@@ -98,17 +98,6 @@ public final class StatusChecks {
           () ->
               !(CTREUtils.unwrap(encoder.getFaultField()) != 0
                   || CTREUtils.unwrap(encoder.getStickyFaultField()) != 0));
-    }
-
-    public void add(String name, SparkMax motor) {
-      add(motor.getDeviceId() + ". " + "Alive " + name, () -> motor.getFirmwareVersion() != 0);
-      add(
-          motor.getDeviceId() + ". " + "No Faults " + name,
-          () -> !(motor.getFaults().rawBits != 0 || motor.getStickyFaults().rawBits != 0));
-    }
-
-    public void add(String name, DutyCycleEncoder encoder) {
-      add(name + " Connected", () -> encoder.isConnected());
     }
 
     public void timestampAdd(String name, DoubleSupplier checkSupplier) {
